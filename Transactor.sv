065bb87de14522bdf360d95102fbbba940edd53f
priyanshi-Shisodia <2022eeb1206@iitrpr.ac.in>
2025-01-06 09:03:07 +0530

    Transactor.sv

--- Transactor.sv
+++ Transactor.sv
@@ -1,11 +1,6 @@
-// TRANSACTOR
-class transactor;
-  rand bit r_en;
-  rand bit w_en;
-  rand bit [7:0] wdata;
-  bit [7:0] rdata;
-  bit full;
-  bit empty;
- // constraint rd_wr_en{ r_en != w_en; }
-  
+class transaction;
+rand bit [7:0] din;
+rand bit [7:0] addr;
+bit wr;
+bit [7:0] dout;
 endclass
