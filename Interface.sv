d4ede14ca3f81c5658baf7d262c943a9e5f87cf7
priyanshi-Shisodia <2022eeb1206@iitrpr.ac.in>
2025-01-06 09:04:21 +0530

    Interface.sv

--- Interface.sv
+++ Interface.sv
@@ -1,33 +1,6 @@
-//INTERFACE
-interface fifo_if(input logic clk)
-  logic r_en;
-  logic rst;
-  logic r_en;
-  logic [7:0] wdata;
-  logic [7:0] rdata;
-  logic full,empty;
-  clocking driver_cb@(posedge clk)
-    default input #1 output #1;
-    output rst;
-    output wdata;
-    output r_en;
-    output w_en;
-    input full;
-    input empty;
-    input rdata;
-  endclocking
-  
-  clocking monitor@(posedge clk);
-    input rst;
-    input r_en;
-    input w_en;
-    input rdata;
-    input wdata;
-    input full;
-    input empty;
-  endclocking
-  
-  modport DRIVER(clocking driver_cb,input clk);
-  modport MONITOR(clocking monitor, input clk);
+interface counter_intf();
+logic clk, rst, wr;
+logic [7:0] din, addr;
+logic [7:0] dout;
 endinterface
  
