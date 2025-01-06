3bd52d6f4d8c6b0265bf7cb7d795807b83171100
priyanshi-Shisodia <2022eeb1206@iitrpr.ac.in>
2025-01-06 09:02:06 +0530

    Update FIFO.sv

--- FIFO.sv
+++ FIFO.sv
@@ -1,45 +1,60 @@
-// FIFO Design
-module fifo(clk,rst,rdata,wdata,r_en,w_en,full,empty);
-input clk,rst,w_en,r_en;
-output reg full, empty;
-  input [7:0] wdata;
-  output reg [7:0] rdata;
-  reg [5:0]w_ptr;
-  reg [5:0]r_ptr;
-  reg [7:0] mem [31:0];
-  integer i;
-  
-  //writing data into the FIFO
-  always@(posedge clk,posedge rst)
-    if(rst) begin
-      for(i=0;i<=32;i=i+1)
-        mem[i]<=8'b0;
-    end
-  else if(w_en & ~full)
-    mem[w_ptr]<=wdata;
-  
-  //reading data from FIFO
-  always@(posedge clk,posedge rst)
-    if(rst) begin
-      rdata<=8'b0;
-    end
-  else if(r_en & ~empty)
-    rdata <= mem[r_ptr];
-  
-  //Pointer Block
-  always@(posedge clk,posedge rst)
-    if(rst)
-      w_ptr<=6'b0;
-  else if(w_en & ~full)
-    w_ptr<=w_ptr+1;
-  
-  always@(posedge clk,posedge rst)
-    if(rst)
-      r_ptr<=6'b0;
-  else if(r_en & ~empty)
-    r_ptr<=r_ptr+1;
-  
-  assign empty = (r_ptr==w_ptr);
-  assign full = ((w_ptr[4:0]==r_ptr[4:0]) && (w_ptr[5] != r_ptr[5]));
-  
-                 endmodule
+module fifo (
+input reset, clk, rd, wr,
+input [7:0] din,
+output full, empty,
+output reg [7:0] dout,
+output reg [3:0] fifo_cnt
+);
+
+reg [7:0] fifo_ram [0:7];
+reg [2:0] wr_ptr, rd_ptr;
+
+//Defination of “empty” and “full”
+
+assign empty = (fifo_cnt == 0);
+assign full = (fifo_cnt == 8);
+
+// Write
+always @(posedge clk) begin
+if (wr && !full) begin
+fifo_ram[wr_ptr] <= din;
+wr_ptr <= wr_ptr + 1;
+fifo_cnt <= fifo_cnt + 1;
+end
+end
+
+// Read
+always @(posedge clk) begin
+if (rd && !empty) begin
+dout <= fifo_ram[rd_ptr];
+rd_ptr <= rd_ptr + 1;
+fifo_cnt <= fifo_cnt — 1;
+end
+end
+
+//Pointer Block
+always @(posedge clk or posedge reset) begin
+if (reset) begin
+wr_ptr <= 0;
+rd_ptr <= 0;
+end
+
+else begin
+if (wr && !full || wr && rd) wr_ptr <= wr_ptr + 1;
+if (rd && !empty || rd && wr) rd_ptr <= rd_ptr + 1;
+end
+end
+
+//FIFO Counter
+
+always @(posedge clk or posedge reset) begin
+if (reset)
+fifo_cnt <= 0;
+else
+case ({rd, wr})
+2'b01: fifo_cnt <= (fifo_cnt == 8) ? 8 : fifo_cnt + 1;
+2'b10: fifo_cnt <= (fifo_cnt == 0) ? 0 : fifo_cnt — 1;
+endcase
+end
+
+endmodule
