c1b8385d654c157f3dc009733916ec77ae5c8c5d
priyanshi-Shisodia <2022eeb1206@iitrpr.ac.in>
2025-01-06 09:06:08 +0530

    Scoreboard.sv

--- Scoreboard.sv
+++ Scoreboard.sv
@@ -1,45 +1,35 @@
 class scoreboard;
-  
-mailbox gen2driv;
-mailbox rcvr2sb;
-  integer compare;
-  
-  function new(mailbox gen2driv,mailbox rcvr2sb);
-    this.gen2driv = gen2driv;
-    this.rcvr2sb = rcvr2sb;
-  endfunction :new
-  
-  task start();
-    transactor trans_rcv,trans;
-    trans_rcv = new();
-    fork
-    forever
-    begin
-      rcvr2sb.get(trans_rcv);
-      gen2driv.get(trans);
-      /* $display("==========Scoreboard========");
-      begin
-        compare =1'b0;
-        if(trans.w_en==trans_rcv.w_en && trans.r_en==trans_rcv.r_en)
-          compare =1;
-      end
-      if(trans_rcv.full || trans_rcv.empty) begin
-        if(trans_rcv.full)
-          $display("\w_en=%0h    memory is full",trans_rcv.w_en);
-        else 
-          $display("\w_en=%0h  \wdata=%0h",trans_rcv.w_en,trans_rcv.wdata);
-      end
-      if(trans_rcv.empty)
-        $display("\r_en=%0h    memory is empty",trans_rcv.r_en);
-      else
-        $display("\r_en=%0h  \rdata=%0h",trans_rcv.r_en,trans_rcv.rdata);
-      if(compare==1)
-        $display("Yes");
-      else
-        $display("No");*/
-    end
-    join_none
-  endtask :start
-endclass
+mailbox mbx;
+transaction tarr[256];
+transaction t;
 
-        
+function new(mailbox mbx);
+this.mbx = mbx;
+endfunction
+
+task run();
+forever begin
+mbx.get(t);
+
+if (t.wr == 1'b1) begin
+if (tarr[t.addr] == null) begin
+tarr[t.addr] = new();
+tarr[t.addr] = t;
+$display(“[SCO] : Data stored”);
+end
+end else begin
+if (tarr[t.addr] == null) begin
+if (t.dout == 0)
+$display(“[SCO] : Data read Test Passed”);
+else
+$display(“[SCO] : Data read Test Failed”);
+end else begin
+if (t.dout == tarr[t.addr].din)
+$display(“[SCO] : Data read Test Passed”);
+else
+$display(“[SCO] : Data read Test Failed”);
+end
+end
+end
+endtask
+endclass
